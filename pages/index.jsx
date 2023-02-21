f59fc2a07d7a56395a1baa5f6327097417041e97
Prathamesh Dukare <Prathameshdukaremail@gmail.com>
2023-02-21 18:41:10 +0530

    add Md pages & list blogs with getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,24 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../utils/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      
+
       <section className={utilStyles.headingMd}>
         <p>Hey 👋, I'm Prathamesh,
           I write code and love making useful products which solves real world problems. <br />In my free time I read books, watch documentaries and click some pictures🐬.</p>
@@ -17,6 +27,22 @@ export default function Home() {
           <a href="https://peerlist.io/prathamesh" target="_blank">Peerlist</a>
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
     </Layout>
   );
 }
