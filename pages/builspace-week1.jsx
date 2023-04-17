cfb4611180cb02f829b0577515216f12f4f11ff0
Prathamesh Dukare <Prathameshdukaremail@gmail.com>
2023-04-18 01:01:47 +0530

    add : video post

---
+++ pages/builspace-week1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function BuildSpace() {
+    return (
+        <div >
+            <video style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '50%',
+            }} src="/videos/week1-update.mp4" controls></video>
+        </div>
+    )
+}
