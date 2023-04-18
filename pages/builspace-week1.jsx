4767480f17257c1d145f2c85c81dcb1b84faf0ea
Prathamesh Dukare <Prathameshdukaremail@gmail.com>
2023-04-18 10:13:54 +0530

    update : video post

--- pages/builspace-week1.jsx
+++ pages/builspace-week1.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 
 export default function BuildSpace() {
     return (
-        <div >
-            <video style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '50%',
-            }} src="/videos/week1-update.mp4" controls></video>
+        <div style={{ margin: "0 auto", textAlign: 'center' }}>
+            <h1 style={{ textAlign: 'center' }}>Buildspace week1 update</h1>
+            <a style={{ display: 'block', margin: "1rem auto" }} href="https://youtu.be/nX9wOsnxp_Y">YT link</a>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/nX9wOsnxp_Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     )
 }
