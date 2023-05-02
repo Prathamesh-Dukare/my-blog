bf963c23fea00481d512d1b8859228268f050ddf
Prathamesh Dukare <Prathameshdukaremail@gmail.com>
2023-05-02 15:09:25 +0530

    move blog to typescript

---
+++ pages/index.tsx
@@ -0,0 +1,64 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../utils/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from "next";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
+
+type PostDateType = {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+};
+
+export default function Home({ allPostsData }: PostDateType) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hey 👋, I'm Prathamesh, I write code and love making useful products
+          which solves real world problems. <br />
+          In my free time I read books, watch documentaries and click some
+          pictures🐬.
+        </p>
+        <p>
+          Connect with me on{" "}
+          <a href="https://peerlist.io/prathamesh" target="_blank">
+            Peerlist
+          </a>
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
